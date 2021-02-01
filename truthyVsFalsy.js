6a1170507074b8ba4553daad75f762eed561d7ed
Munjurul-Islam <munjurulislam94@gmail.com>
2021-02-01 23:22:38 +0600

    true vs false

---
+++ truthyVsFalsy.js
@@ -0,0 +1,13 @@
+// falsy condition
+// 0, NaN, null, '', undifine, false
+// truthy condition
+// '0', ' ', {}, [], 'false'
+
+let number = ''; 
+
+if(number){
+    console.log('this is true');
+}
+else{
+    console.log('this is false');
+}
