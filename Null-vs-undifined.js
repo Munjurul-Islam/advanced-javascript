cc06b17339be27ef4a8d6169535804a3b94a22e1
Munjurul-Islam <munjurulislam94@gmail.com>
2021-02-01 23:56:51 +0600

    discuss null vs undifined for interview question

---
+++ Null-vs-undifined.js
@@ -0,0 +1,21 @@
+let name;                   /**undifined value */
+console.log(name);
+
+let number = [3, 4, 5];
+console.log(number[6])           /**[6]position is undifined this arry */
+
+let friends = {name: 'amir', age: 27, };
+console.log(friends.phone);              /**phone property is not difined this object */
+
+function add(num1, num2){
+    console.log(num1 + num2)
+                                         /**you have to return value or not undifined output showing */
+}
+const result = add(2,3);
+console.log(result)
+
+
+
+
+// Null  is set by developer
+// where's there is no value
